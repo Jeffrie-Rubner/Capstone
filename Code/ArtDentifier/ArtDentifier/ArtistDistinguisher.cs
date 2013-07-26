94ea1b39dd646d0e07a66455a86ae886c3deb632
Jeffrie Rubner <revenant.ghost@hotmail.com>
2013-07-26 15:00:10 -0600

    Progress towards first metric
    The sample images chosen are committed to the documentation.  Errors
    exist in the code that keep it from running.  Images are not being read
    in and added to the dictionary.

--- Code/ArtDentifier/ArtDentifier/ArtistDistinguisher.cs
+++ Code/ArtDentifier/ArtDentifier/ArtistDistinguisher.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 /*
  * This class will make use of metrics to distinguish between artists
@@ -17,18 +18,26 @@ namespace ArtDentifier
         private List<BitmapImage> RaphaelPieces = new List<BitmapImage>();
         private List<BitmapImage> BlakePieces = new List<BitmapImage>();
         private List<BitmapImage> VanGoghPieces = new List<BitmapImage>();
+        private ColorScaleAspectDeterminer colorSAD = new ColorScaleAspectDeterminer();
 
         public ArtistDistinguisher()
         {
-            AddArtistsToDictionary();
-            InitializeArtists();
+          AddArtistsToDictionary();
+         //   InitializeArtists();
         }
 
         public void AnalyzePicture(BitmapImage bitmap)
         {
            //create a method that allows images to be compared to each artists image on every metric
+            Color c = colorSAD.determineBitMean(bitmap);
+            foreach (KeyValuePair<String, List<BitmapImage>> kvp in allArtists)
+            {
+
+            }
         }
         
+
+
         #region InitializationMethods
         private void AddArtistsToDictionary()
         {
@@ -40,9 +49,11 @@ namespace ArtDentifier
         }
         private void InitializeArtists()
         {
-            foreach (List<BitmapImage> bmi in allArtists.Values)
+            foreach (KeyValuePair<String, List<BitmapImage>> kvp in allArtists)
             {
-                //initialize each list with at least 5 pieces of art
+                Uri artistLocation = new Uri(@"/TeachingImages/" + kvp.Key);
+
+                //kvp.Value.Add(new BitmapImage());    
             }
         }
         #endregion
