bb7be7e667ec5cd3157e5e75c7d47e58a06c2c09
Jeffrie Rubner <revenant.ghost@hotmail.com>
2013-09-08 16:07:06 -0600

    Image Grabbing
    created methods to grab images. The images will then need to be
    clustered.

---
+++ Code/ProperArtdentifier/ProperArtdentifier/Clusterer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace ProperArtdentifier
+{
+    class Clusterer
+    {
+        List<BitmapImage> imagesToCluster;
+
+        public Clusterer()
+        {
+            imagesToCluster = new List<BitmapImage>();
+        }
+
+        public void Cluster()
+        {
+
+        }
+
+        public void ReadInData()
+        {
+            string dir = @"c:\Users\Jeff\Documents\GitHub\Capstone\Documentation\Misc\TeachingImages\";
+            string[] filePaths = Directory.GetFiles(dir);
+            foreach (string str in filePaths)
+            {
+                Uri artistLocation = new Uri(str);
+                try
+                {
+                    BitmapImage myBitmapImage = GetBitmapImage(artistLocation);
+                    imagesToCluster.Add(myBitmapImage);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.StackTrace);
+                }
+            }
+        }
+
+        private BitmapImage GetBitmapImage(Uri uri)
+        {
+            BitmapImage myBitmapImage = new BitmapImage();
+            myBitmapImage.BeginInit();
+            myBitmapImage.UriSource = uri;
+            myBitmapImage.EndInit();
+            return myBitmapImage;
+        }
+
+    }
+}
